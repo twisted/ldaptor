daa4d34bdddcddc3302f02416c6081d848472f1f
tv <tv@373aa48d-36e5-0310-bb30-ae74d9883905>
2004-12-16 11:17:49 +0000

    Extract common DNS utility functions to ldaptor.dns.
    git-svn-id: svn+ssh://open.inoi.fi/open-ldaptor/trunk@164 373aa48d-36e5-0310-bb30-ae74d9883905

---
+++ ldaptor/dns.py
@@ -0,0 +1,71 @@
+"""DNS-related utilities."""
+
+from socket import inet_aton, inet_ntoa
+
+def aton_octets(ip):
+    s=inet_aton(ip)
+    octets=map(None, s)
+    n=0L
+    for o in octets:
+	n=n<<8
+	n+=ord(o)
+    return n
+
+def aton_numbits(num):
+    n=0L
+    while num>0:
+	n>>=1
+	n |= 2**31
+	num-=1
+    return n
+
+def aton(ip):
+    try:
+	i=int(ip)
+    except ValueError:
+	return aton_octets(ip)
+    else:
+	return aton_numbits(i)
+
+def ntoa(n):
+    s=(
+	chr((n>>24)&0xFF)
+	+ chr((n>>16)&0xFF)
+	+ chr((n>>8)&0xFF)
+	+ chr(n&0xFF)
+       )
+    ip=inet_ntoa(s)
+    return ip
+
+def netmaskToNumbits(netmask):
+    bits = aton(netmask)
+    i = 2**31
+    n = 0
+    while bits and i > 0:
+        if (bits & i) == 0:
+            if bits:
+                raise RuntimeError, "Invalid netmask: %s" % netmask
+        n += 1
+        bits -= i
+        i = i >> 1
+    return n
+
+def ptrSoaName(ip, netmask):
+    """
+    Convert an IP address and netmask to a CIDR delegation
+    -style zone name.
+    """
+    net = aton(ip) & aton(netmask)
+
+    nmBits = netmaskToNumbits(netmask)
+    bytes, bits = divmod(nmBits, 8)
+    octets = ntoa(net).split('.')
+    octets.reverse()
+    if not bits:
+        octets = octets[-bytes:]
+    else:
+        partial = octets[-bytes-1]
+        octets = octets[-bytes:]
+        octets.insert(0, '%s/%d' % (partial, nmBits))
+
+    return '.'.join(octets)+'.in-addr.arpa.'
---
+++ ldaptor/test/test_dns.py
@@ -0,0 +1,79 @@
+"""
+Test cases for ldaptor.dns
+"""
+
+from twisted.trial import unittest
+from ldaptor import dns
+
+class NetmaskToNumbits(unittest.TestCase):
+    def test_classA(self):
+        self.assertEquals(dns.netmaskToNumbits('255.0.0.0'), 8)
+
+    def test_classB(self):
+        self.assertEquals(dns.netmaskToNumbits('255.255.0.0'), 16)
+
+    def test_classC(self):
+        self.assertEquals(dns.netmaskToNumbits('255.255.255.0'), 24)
+
+    def test_host(self):
+        self.assertEquals(dns.netmaskToNumbits('255.255.255.255'), 32)
+
+    def test_numbits(self):
+        for i in range(0, 33):
+            self.assertEquals(dns.netmaskToNumbits(str(i)), i)
+
+    def test_CIDR(self):
+        for i in range(0, 33):
+            mask = dns.ntoa(dns.aton(i))
+            self.assertEquals(dns.netmaskToNumbits(mask), i)
+
+class PtrSoaName(unittest.TestCase):
+    def test_classA(self):
+        self.assertEquals(dns.ptrSoaName('1.2.3.4', '255.0.0.0'),
+                          '1.in-addr.arpa.')
+        self.assertEquals(dns.ptrSoaName('1.2.3.4', '8'),
+                          '1.in-addr.arpa.')
+
+    def test_classB(self):
+        self.assertEquals(dns.ptrSoaName('1.2.3.4', '255.255.0.0'),
+                          '2.1.in-addr.arpa.')
+        self.assertEquals(dns.ptrSoaName('1.2.3.4', '16'),
+                          '2.1.in-addr.arpa.')
+
+    def test_classC(self):
+        self.assertEquals(dns.ptrSoaName('1.2.3.4', '255.255.255.0'),
+                          '3.2.1.in-addr.arpa.')
+        self.assertEquals(dns.ptrSoaName('1.2.3.4', '24'),
+                          '3.2.1.in-addr.arpa.')
+
+    def test_CIDR_9(self):
+        self.assertEquals(dns.ptrSoaName('1.2.3.4', '9'),
+                          '0/9.1.in-addr.arpa.')
+        self.assertEquals(dns.ptrSoaName('1.200.3.4', '9'),
+                          '128/9.1.in-addr.arpa.')
+
+    def test_CIDR_12(self):
+        self.assertEquals(dns.ptrSoaName('1.2.3.4', '12'),
+                          '0/12.1.in-addr.arpa.')
+        self.assertEquals(dns.ptrSoaName('1.200.3.4', '12'),
+                          '192/12.1.in-addr.arpa.')
+
+    def test_CIDR_13(self):
+        self.assertEquals(dns.ptrSoaName('1.2.3.4', '13'),
+                          '0/13.1.in-addr.arpa.')
+        self.assertEquals(dns.ptrSoaName('1.200.3.4', '13'),
+                          '200/13.1.in-addr.arpa.')
+
+    def test_CIDR_15(self):
+        self.assertEquals(dns.ptrSoaName('1.2.3.4', '15'),
+                          '2/15.1.in-addr.arpa.')
+        self.assertEquals(dns.ptrSoaName('1.200.3.4', '15'),
+                          '200/15.1.in-addr.arpa.')
+
+    def test_CIDR_29(self):
+        self.assertEquals(dns.ptrSoaName('1.2.3.4', '29'),
+                          '0/29.3.2.1.in-addr.arpa.')
+
+    def test_CIDR_30(self):
+        self.assertEquals(dns.ptrSoaName('1.2.3.4', '30'),
+                          '4/30.3.2.1.in-addr.arpa.')
