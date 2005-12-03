b4b9a0b10269dcd5c6712dbce35c526adfa4636e
tv <tv@373aa48d-36e5-0310-bb30-ae74d9883905>
2005-12-03 21:39:31 +0000

    Cleanup.
    git-svn-id: svn+ssh://open.inoi.fi/open-ldaptor/trunk@320 373aa48d-36e5-0310-bb30-ae74d9883905

--- ldaptor/protocols/ldap/ldaperrors.py
+++ ldaptor/protocols/ldap/ldaperrors.py
@@ -14,6 +14,10 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+# make pyflakes not complain about undefined names
+reverse = None
+LDAPOther = None
+
 def get(resultCode, errorMessage):
     """Get an instance of the correct exception for this resultCode."""
 
