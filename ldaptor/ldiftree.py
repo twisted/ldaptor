be3ed01b83503b5ca350edafc63755a2f2f1e191
tv <tv@373aa48d-36e5-0310-bb30-ae74d9883905>
2005-12-03 14:36:41 +0000

    Share diffTree implementation.
    git-svn-id: svn+ssh://open.inoi.fi/open-ldaptor/trunk@317 373aa48d-36e5-0310-bb30-ae74d9883905

--- ldaptor/ldiftree.py
+++ ldaptor/ldiftree.py
@@ -103,7 +103,8 @@ def _put(path, entry):
 def put(path, entry):
     return defer.execute(_put, path, entry)
 
-class LDIFTreeEntry(entry.EditableLDAPEntry):
+class LDIFTreeEntry(entry.EditableLDAPEntry,
+                    entry.DiffTreeMixin):
     __implements__ = (interfaces.IConnectedLDAPEntry,
                       )
     def __init__(self, path, dn=None, *a, **kw):
@@ -427,112 +428,6 @@ class LDIFTreeEntry(entry.EditableLDAPEntry):
 
     def deleteChild(self, rdn):
         return defer.maybeDeferred(self._deleteChild, rdn)
-
-    def _diffTree_gotMyChildren(self, myChildren, other, result):
-        d = other.children()
-        d.addCallback(self._diffTree_gotBothChildren, myChildren, other, result)
-        return d
-
-    def _diffTree_gotBothChildren(self,
-                                  otherChildren,
-                                  myChildren,
-                                  other,
-                                  result):
-        def rdnToChild(rdn, l):
-            r = [x for x in l if x.dn.split()[0] == rdn]
-            assert len(r) == 1
-            return r[0]
-
-        my = sets.Set([x.dn.split()[0] for x in myChildren])
-        his = sets.Set([x.dn.split()[0] for x in otherChildren])
-
-        # differences in common children
-        commonRDN = list(my & his)
-        commonRDN.sort() # for reproducability only
-        d = self._diffTree_commonChildren([
-            (rdnToChild(rdn, myChildren), rdnToChild(rdn, otherChildren))
-            for rdn in commonRDN
-            ], result)
-
-        # added children
-        addedRDN = list(his - my)
-        addedRDN.sort() # for reproducability only
-        d2 = self._diffTree_addedChildren([
-            rdnToChild(rdn, otherChildren)
-            for rdn in addedRDN
-            ], result)
-        d.addCallback(lambda _: d2)
-
-        # deleted children
-        deletedRDN = list(my - his)
-        deletedRDN.sort() # for reproducability only
-        d3 = self._diffTree_deletedChildren([
-            rdnToChild(rdn, myChildren)
-            for rdn in deletedRDN
-            ], result)
-        d.addCallback(lambda _: d3)
-
-        return d
-
-    def _diffTree_commonChildren(self, children, result):
-        if not children:
-            return defer.succeed(result)
-        first, rest = children[0], children[1:]
-        a, b = first
-        d = a.diffTree(b, result)
-        d.addCallback(lambda _: self._diffTree_commonChildren(rest, result))
-        return d
-
-    def _diffTree_addedChildren(self, children, result):
-        if not children:
-            return result
-        first, rest = children[0], children[1:]
-
-        d = first.subtree()
-        def _gotSubtree(l, result):
-            for c in l:
-                o = delta.AddOp(c)
-                result.append(o)
-            return result
-        d.addCallback(_gotSubtree, result)
-
-        d.addCallback(lambda _: self._diffTree_addedChildren(rest, result))
-        return d
-
-    def _diffTree_deletedChildren(self, children, result):
-        if not children:
-            return result
-        first, rest = children[0], children[1:]
-
-        d = first.subtree()
-        def _gotSubtree(l, result):
-            l.reverse() # remove children before their parent
-            for c in l:
-                o = delta.DeleteOp(c)
-                result.append(o)
-            return result
-        d.addCallback(_gotSubtree, result)
-
-        d.addCallback(lambda _: self._diffTree_deletedChildren(rest, result))
-        return d
-
-    def diffTree(self, other, result=None):
-        assert self.dn == other.dn, \
-               ("diffTree arguments must refer to same LDAP tree:"
-                "%r != %r" % (str(self.dn), str(other.dn))
-                )
-        if result is None:
-            result = []
-
-        # differences in root
-        rootDiff = self.diff(other)
-        if rootDiff is not None:
-            result.append(rootDiff)
-
-        d = self.children()
-        d.addCallback(self._diffTree_gotMyChildren, other, result)
-            
-        return d
 
     def __repr__(self):
         return '%s(%r, %r)' % (self.__class__.__name__,
