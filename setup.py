a1d3f7d55eb88dfb2f99253f3390076e7794fdd0
Bret Curtis <psi29a@gmail.com>
2014-07-04 15:22:34 +0200

    working pypi setup and prep work for 0.54

--- setup.py
+++ setup.py
@@ -1,65 +1,48 @@
 #!/usr/bin/python
+import os
+from setuptools import setup
 
-import os, errno
-from distutils.core import setup
-from distutils.command.build import build
-from distutils.command.clean import clean
-from distutils.command.install import install
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     setup(name="ldaptor",
-	  description="Pure-Python library for LDAP",
-	  long_description="""
-
-Ldaptor is a pure-Python library that implements
-
-- LDAP client logic.
-
-- separately-accessible LDAP and BER protocol message
-generation/parsing.
-
-- ASCII-format LDAP filter generation and parsing.
-
-- LDIF format data generation.
-
-- Samba password changing logic.
-
-Also included is a web-based user interface to search and edit
-information in an LDAP directory and a set of LDAP utilities for use
-from the command line.
-
-""".strip(),
-	  author="Tommi Virtanen",
-	  author_email="tv@eagain.net",
-	  #url="TODO",
-	  license="MIT",
-
-          cmdclass={'build': build,
-                    'clean': clean,
-                    'install': install,
-                    },
-
-	  packages=[
-	"ldaptor",
-	"ldaptor.protocols",
-        "ldaptor.protocols.ldap",
-        "ldaptor.protocols.ldap.autofill",
-	"ldaptor.samba",
-	"ldaptor.test",
-	],
-	  scripts=[
-	"bin/ldaptor-ldap2dhcpconf",
-	"bin/ldaptor-ldap2maradns",
-	"bin/ldaptor-ldap2dnszones",
-	"bin/ldaptor-search",
-	"bin/ldaptor-namingcontexts",
-	"bin/ldaptor-passwd",
-	"bin/ldaptor-ldap2passwd",
-	"bin/ldaptor-getfreenumber",
-	"bin/ldaptor-ldap2pdns",
-	"bin/ldaptor-find-server",
-	"bin/ldaptor-rename",
-	"bin/ldaptor-fetchschema",
-        "bin/ldaptor-ldifdiff",
-        "bin/ldaptor-ldifpatch",
-        ])
+          version='0.0.54',
+          description="Pure-Python library for LDAP",
+          long_description=read('README.md'),
+          author="Tommi Virtanen",
+          author_email="tv@eagain.net",
+          maintainer="Bret Curtis",
+          maintainer_email="psi29a@gmail.com",
+          url="https://github.com/twisted/ldaptor",
+          license="MIT",
+          install_requires=['zope.interface', 'Twisted', 'pyparsing', 'pyOpenSSL', 'PyCrypto'],
+          classifiers=['Intended Audience :: Developers',
+                       'License :: OSI Approved :: MIT License',
+                       'Operating System :: OS Independent',
+                       'Development Status :: 7 - Inactive',
+                       'Framework :: Twisted', 'Programming Language :: Python',
+                       'Topic :: Software Development :: Libraries :: Python Modules',
+                       'Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP'],
+          packages=["ldaptor",
+                    "ldaptor.protocols",
+                    "ldaptor.protocols.ldap",
+                    "ldaptor.protocols.ldap.autofill",
+                    "ldaptor.samba",
+                    "ldaptor.test"],
+          scripts=["bin/ldaptor-ldap2dhcpconf",
+                   "bin/ldaptor-ldap2maradns",
+                   "bin/ldaptor-ldap2dnszones",
+                   "bin/ldaptor-search",
+                   "bin/ldaptor-namingcontexts",
+                   "bin/ldaptor-passwd",
+                   "bin/ldaptor-ldap2passwd",
+                   "bin/ldaptor-getfreenumber",
+                   "bin/ldaptor-ldap2pdns",
+                   "bin/ldaptor-find-server",
+                   "bin/ldaptor-rename",
+                   "bin/ldaptor-fetchschema",
+                   "bin/ldaptor-ldifdiff",
+                   "bin/ldaptor-ldifpatch"]
+          )
