72e6f3cc944680744f85816776be6eeaccf07c84
Aaron Peschel <apeschel@foxpass.com>
2016-07-05 13:57:54 -0700

    Implement Compare* Functionity in LDAPServer
    This commit implements both the CompareRequest and CompareResponse
    functionality for the LDAP Server.

--- ldaptor/protocols/ldap/ldapserver.py
+++ ldaptor/protocols/ldap/ldapserver.py
@@ -196,44 +196,66 @@ class LDAPServer(BaseLDAPServer):
         return pureldap.LDAPSearchResultDone(
             resultCode=ldaperrors.Success.resultCode)
 
-    def _cbCompareGotBase(self, base, dn, request, reply):
-        d = base.search()
-
-        def _done(_):
-            return pureldap.LDAPCompareResponse(
-                    resultCode=ldaperrors.LDAPNoSuchObject.resultCode)
-        d.addCallback(_done)
-
-        return d
-
-    def _cbCompareLDAPError(self, reason):
-        reason.trap(ldaperrors.LDAPException)
-        return pureldap.LDAPCompareResponse(
-            resultCode=reason.value.resultCode)
-
-    def _cbCompareOtherError(self, reason):
-        return pureldap.LDAPCompareResponse(
-            resultCode=ldaperrors.other,
-            errorMessage=reason.getErrorMessage())
-
     fail_LDAPCompareRequest = pureldap.LDAPCompareResponse
 
     def handle_LDAPCompareRequest(self, request, controls, reply):
         self.checkControls(controls)
 
-        if (request.entry == ''
-                and request.scope == pureldap.LDAP_SCOPE_baseObject
-                and request.filter == pureldap.LDAPFilter_present('objectClass')):
-            return self.getRootDSE(request, reply)
-
         dn = distinguishedname.DistinguishedName(request.entry)
-        root = interfaces.IConnectedLDAPEntry(self.factory)
 
+        root = interfaces.IConnectedLDAPEntry(self.factory)
         d = root.lookup(dn)
         d.addCallback(self._cbCompareGotBase, dn, request, reply)
         d.addErrback(self._cbCompareLDAPError)
         d.addErrback(defer.logError)
         d.addErrback(self._cbCompareOtherError)
+        return d
+
+    def handle_LDAPCompareRequest(self, request, controls, reply):
+        def _cbCompareGotBase(base, ava, reply):
+            def _done(result_list):
+                if result_list:
+                    resultCode = ldaperrors.LDAPCompareTrue.resultCode
+                else:
+                    resultCode = ldaperrors.LDAPCompareFalse.resultCode
+                return pureldap.LDAPCompareResponse(resultCode)
+
+            # base.search only works with Filter Objects, and not with
+            # AttributeValueAssertion objects. Here we convert the AVA to an
+            # equivalent Filter so we can re-use the existing search
+            # functionality we require.
+            search_filter = pureldap.LDAPFilter_equalityMatch(
+                attributeDesc=ava.attributeDesc,
+                assertionValue=ava.assertionValue
+            )
+
+            d = base.search(filterObject=search_filter,
+                           scope=pureldap.LDAP_SCOPE_baseObject,
+                           derefAliases = pureldap.LDAP_DEREF_neverDerefAliases)
+
+            d.addCallback(_done)
+
+            return d
+
+        def _cbCompareLDAPError(reason):
+            reason.trap(ldaperrors.LDAPException)
+            return pureldap.LDAPCompareResponse(
+                resultCode=reason.value.resultCode)
+
+        def _cbCompareOtherError(reason):
+            return pureldap.LDAPCompareResponse(
+                resultCode=ldaperrors.other,
+                errorMessage=reason.getErrorMessage())
+
+        self.checkControls(controls)
+        dn = distinguishedname.DistinguishedName(request.entry)
+        root = interfaces.IConnectedLDAPEntry(self.factory)
+
+        d = root.lookup(dn)
+        d.addCallback(_cbCompareGotBase, request.ava, reply)
+        d.addErrback(_cbCompareLDAPError)
+        d.addErrback(defer.logError)
+        d.addErrback(_cbCompareOtherError)
         return d
 
     def _cbSearchGotBase(self, base, dn, request, reply):
