023dc855e928d7881d7d9103d3ba54891aff5c02
Thomas Grainger <tagrain@gmail.com>
2019-03-04 13:29:02 +0000

    avoid assert in loop
    allows all tests to run, and shows which failed

--- ldaptor/test/test_smbpassword.py
+++ ldaptor/test/test_smbpassword.py
@@ -10,29 +10,28 @@ class TestNTHash(unittest.TestCase):
     """
     Unit tests for smbpassword.nthash.
     """
-    knownValues=( # password, expected_result
+    knownValues=[ # password, expected_result
         (b'', b'31D6CFE0D16AE931B73C59D7E0C089C0'),
         (b'foo', b'AC8E657F83DF82BEEA5D43BDAF7800CC'),
         (127 * b'x', b'25900FAB94A048BCF438615217776562'),
         (128 * b'x', b'65681023D0CB5E7E96FF662150EF060D'),
         (129 * b'x', b'65681023D0CB5E7E96FF662150EF060D'),
         (1000 * b'x', b'65681023D0CB5E7E96FF662150EF060D'),
-        )
+    ]
 
     def testKnownValues(self):
         """nthash(...) gives known results"""
-        for password, expected in self.knownValues:
-
-            result = smbpassword.nthash(password)
-
-            self.assertEqual(expected, result)
+        self.assertEqual(
+            self.knownValues,
+            [(p, smbpassword.nthash(p)) for p, _ in self.knownValues]
+        )
 
 
 class TestLMHash(unittest.TestCase):
     """
     Unit tests for smbpassword.lmhash.
     """
-    knownValues=( # password, expected_result
+    knownValues=[ # password, expected_result
         (b'', b'AAD3B435B51404EEAAD3B435B51404EE'),
         (b'foo', b'5BFAFBEBFB6A0942AAD3B435B51404EE'),
         (13 * b'x', b'3AA62DBBEFDB676366B4159AF5A7C45C'),
@@ -42,7 +41,7 @@ class TestLMHash(unittest.TestCase):
         (b'1234567abcdefg', b'0182BD0BD4444BF8E0C510199CC66ABD'),
         (b'XXXXXXXabcdefg', b'3AA62DBBEFDB6763E0C510199CC66ABD'),
         (b'1234567XXXXXXX', b'0182BD0BD4444BF83AA62DBBEFDB6763'),
-        )
+    ]
 
     def testKnownValues(self):
         """
@@ -51,11 +50,10 @@ class TestLMHash(unittest.TestCase):
         """
         cfg = config.loadConfig()
         cfg.set('samba', 'use-lmhash', 'yes')
-        for password, expected in self.knownValues:
-
-            result = smbpassword.lmhash(password)
-
-            self.assertEqual(expected, result)
+        self.assertEqual(
+            self.knownValues,
+            [(p, smbpassword.lmhash(p)) for p, _ in self.knownValues]
+        )
 
     def testLMHashConfiguationDisabled(self):
         """
